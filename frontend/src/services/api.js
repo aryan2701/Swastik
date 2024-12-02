a0d90d6458a01056061f28c83de0bc90d27f62f5
aryan2701 <101573545+aryan2701@users.noreply.github.com>
2024-12-02 17:08:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://52.66.238.49:4000/api' });
+const API = axios.create({ baseURL: 'http://3.110.166.73:4000/api' });
 
 // Interceptor to attach the token to requests if logged in
 API.interceptors.request.use((req) => {

c075de9f14e6830ba12217ae7974571bad6f7318
aryan2701 <101573545+aryan2701@users.noreply.github.com>
2024-12-10 18:46:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const cors = require('cors'); // Import CORS
+//const cors = require('cors'); // Import CORS
 
 // Load environment variables
 dotenv.config();
